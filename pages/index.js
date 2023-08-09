f5bffaef741f67e683f2fc20a49ddb921e329b17
David Okaliwe <okaliwedavid@gmail.com>
2023-08-09 20:30:51 +0100

    Update

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main className='bg-white px-10'>
         <section className='min-h-screen'>
           <nav className='py-10 mb-12 flex justify-between'>
-            <h1 className='text-xl'>developedbydave</h1>
+            <h1 className='text-xl font-burtons'>developedbydave</h1>
             <ul className='flex items-center'>
               <li>
                 <BsFillMoonStarsFill className='cursor-pointer text-2xl' />
@@ -28,6 +28,16 @@ export default function Home() {
               </li>
             </ul>
           </nav>
+
+          <div>
+            <h2>David Okaliwe</h2>
+            <h3>Developer and Designer</h3>
+            <p>
+              Experienced top-notch developer and creative designer with
+              over 6 years of experience of providing amazing services.
+              Join me below let's get to it!
+            </p>
+          </div>
         </section>
       </main>
     </div>

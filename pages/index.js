d0a0507c6005b9698a2774f80004fb05ab8e170c
David Okaliwe <okaliwedavid@gmail.com>
2023-08-10 23:40:50 +0100

    Update

--- pages/index.js
+++ pages/index.js
@@ -93,7 +93,7 @@ export default function Home() {
             </p>
             <p className='text-md py-2 leading-8 text-gray-800 dark:text-gray-200'>
               I offer a wide range of services, including brand design, 
-              programming and teaching. 
+              programming and teaching.
             </p>
           </div>
 

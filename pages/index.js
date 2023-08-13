e32907aa805926b43017eda5cee00d55fc3150e9
David Okaliwe <okaliwedavid@gmail.com>
2023-08-13 22:12:38 +0100

    Update

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,6 @@ import web4 from '../public/web4.png';
 import web5 from '../public/web5.png';
 import web6 from '../public/web6.png';
 
-
 export default function Home() {
 
   const [darkMode, setDarkMode] = useState(false);

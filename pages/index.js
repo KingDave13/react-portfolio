7cf8d0e33e19b042e82ea47e80f8a975cfc01dab
David Okaliwe <okaliwedavid@gmail.com>
2023-08-15 22:59:48 +0100

    Update

--- pages/index.js
+++ pages/index.js
@@ -194,7 +194,6 @@ export default function Home() {
                 src={web4}
               />
             </div>
-  
             <div className='basis-1/3 flex-1'>
               <Image
                 className='rounded-lg object-cover'
